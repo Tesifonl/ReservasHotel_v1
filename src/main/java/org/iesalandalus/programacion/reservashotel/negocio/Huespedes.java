aa48a6af77fe480f874990e575bdcb661d59fe0f
Tesifonl <tesifonl@gmail.com>
2024-01-10 19:15:41 +0100

    Clase Huespedes

--- src/main/java/org/iesalandalus/programacion/reservashotel/negocio/Huespedes.java
+++ src/main/java/org/iesalandalus/programacion/reservashotel/negocio/Huespedes.java
@@ -1,10 +1,63 @@
 package org.iesalandalus.programacion.reservashotel.negocio;
 
+
+import org.iesalandalus.programacion.reservashotel.dominio.Huesped;
+
 public class Huespedes {
 	
 	private int capacidad;
 	private int tamaño;
+	private static Huesped [] huespedes;
+	
+	public Huespedes(int capacidad) {
+		
+		huespedes=new Huesped [capacidad];
+	}
 	
 	
+	public Huesped [] get() {
+		
+		return huespedes;
+	}
+	
+	
+	private Huesped [] copiaProfundaHuespedes() {
+		
+		Huesped [] copiahuespedes=new Huesped [huespedes.length];
+		
+		for (int i=0;i<huespedes.length;i++) {
+		copiahuespedes[i]=new Huesped(huespedes[i]);
+		}
+		return copiahuespedes;
+	}
 
+	public int getTamaño () {
+		
+		for (int i=0;i<huespedes.length;i++) {
+		if(huespedes[i]!=null) {tamaño++;}
+		else {System.out.println("Nulo");};
+		}
+		return tamaño;
+	}
+	
+	public int getCapacidad () {
+		
+		return huespedes.length;
+	
+	}
+
+	public void insertar (Huesped huesped) {
+		
+		for (int i=0;i<huespedes.length;i++) {
+		if(huespedes[i].equals(huesped)) {System.out.println("Huesped ya incluido en le array");}
+		else {huespedes[getTamaño()+1]=huesped;};
+		}
+	}
+	
+	public Huesped buscar(Huesped huesped) {
+	
+		for (int i=0;i<huespedes.length;i++) {
+		if(huespedes[i].equals(huesped)) {return huesped;}
+		else {return null;}}
+	}
 }
