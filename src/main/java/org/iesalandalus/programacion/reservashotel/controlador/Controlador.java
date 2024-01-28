8afd659bb8beb38b7eb5e10e957e63084b4363ac
Tesifonl <tesifonl@gmail.com>
2024-01-28 19:34:17 +0100

    pendiente vista y controlador

---
+++ src/main/java/org/iesalandalus/programacion/reservashotel/controlador/Controlador.java
@@ -0,0 +1,11 @@
+package org.iesalandalus.programacion.reservashotel.controlador;
+
+import org.iesalandalus.programacion.reservashotel.modelo.Modelo;
+import org.iesalandalus.programacion.reservashotel.vista.Vista;
+
+public class Controlador {
+	
+	private Modelo modelo;
+	private Vista vista;
+
+}
---
+++ src/main/java/org/iesalandalus/programacion/reservashotel/vista/Vista.java
@@ -0,0 +1,5 @@
+package org.iesalandalus.programacion.reservashotel.vista;
+
+public class Vista {
+
+}
